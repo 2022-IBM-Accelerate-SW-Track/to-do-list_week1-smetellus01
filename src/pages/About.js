f2ba37877b931fecb393c0cc7d81a1eb0a0bc6e4
Sebastian Metellus <65518883+smetellus01@users.noreply.github.com>
2022-06-15 15:26:29 -0400

    Update About.js
    finished about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import seb_profile from "../assets/profile.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={seb_profile}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Sebastian Metellus</div>
+        <div className="brief_description">
+          I'm a sophomore studying Computer Science at Florida State University and I aspire to become a software engineer!
+          In my freetime, I enjoy playing ultimate frisbee with friends and drawing. I'm involced in multiple clubs on campus including
+          Blacks in Computing, NSBE, and Black Student Union.
+        </div>
+      </div>
+    </div>
       </div>
     )
   }
